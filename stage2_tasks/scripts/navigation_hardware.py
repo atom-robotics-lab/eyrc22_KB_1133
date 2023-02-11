e9b481662ffe1a3f734949a99c438b97720a41fb
topguns837 <kharjun4@gmail.com>
2023-02-12 00:30:47 +0530

    new script for hardware

---
+++ stage2_tasks/scripts/navigation_hardware.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+class kb_navigation:
+    def __init__(self):
+        self.regions_= {
+        'right': 0,
+        'fright': 0,
+        'front': 0,
+        'fleft': 0,
+        'left': 0
+        }
+
+        self.linear_velocity = 0.7
+        self.kp = 1
+        self._State = 0
+
+        self.message = Twist
+
+        rospy.init_node('kb_navigation')
+        self.pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)        
+        self.sub = rospy.Subscriber('ebot/laser/scan', LaserScan, self.clbk_laser)
+
+    def clbk_laser(msg,self):
+        laser_data = list(msg.ranges)
+        
+        for i in range(len(msg.ranges)):
+            if(laser_data[i] <= 0.1):
+                laser_data[i] = 100.0
+
+        self.regions_ = {
+        'right':  min(min(laser_data[0:106]), 8.0),
+        'fright': min(laser_data[44], 10),
+        'front':  min(min(laser_data[221:309]), 8.0),
+        'fleft':  min(laser_data[134], 10),
+        'left':   min(min(laser_data[434:531]), 8.0),
+        }
+
+        self.take_action()
+
+    def move(self,linear,angular):
+        velocity_msg = Twist()
+        velocity_msg.linear.x = linear
+        velocity_msg.angular.z = angular 
+        self.pub_.publish(velocity_msg)
+
+    def take_action(self):
+        position_error = self.regions_['left'] - self.regions_['right']
+
+        if position_error == 0 :
+            self._State = 0 
+
+        else :
+            self.angular_velocity = self.kp*position_error
+            self._State = 1
+
+
+'''def main():
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():'''
+
+    
+
+
+if __name__ == '__main__':
+    kb_nav = kb_navigation()
+    kb_nav.move(kb_nav.linear_velocity , 0 )
+    kb_nav.main()
+ 
+
